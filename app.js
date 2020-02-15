474e7db7c2ebabefa06a79bf810a6e5d8056500f
Andre Entrekin <andre.entrekin@gmail.com>
2020-02-15 15:28:15 -0600

    cleaning code

--- app.js
+++ app.js
@@ -1,84 +1,82 @@
-const Employee = require("./lib/Employee")
-const Manager = require("./lib/Manager")
-const Engineer = require("./lib/Engineer")
-const Intern = require("./lib/Intern")
-const fs = require("fs")
-const util = require("util")
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const fs = require("fs");
+const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
 const appendFileAsync = util.promisify(fs.appendFile);
-const inquirer = require("inquirer")
-const path = require("path")
+const inquirer = require("inquirer");
+const path = require("path");
 const templatesDir = path.resolve(__dirname, "./test.html");
-
 
 console.log(templatesDir);
 const collectInputs = async (inputs = []) => {
-    const prompts = [
-        {
-            type: "list",
-            message: "What is the employee's role?",
-            name: "role",
-            choices: ["Manager", "Engineer", "Intern"]
-        },
-        {
-            type: "input",
-            message: "What is the employee's name?",
-            name: "name"
-        },
-        {
-            type: "input",
-            message: "What is the employee's ID?",
-            name: "id"
-        },
-        {
-            type: "input",
-            message: "What is the employee's email?",
-            name: "email"
-        },
-        {
-            type: "input",
-            message: "What is the employee's office number?",
-            name: "office",
-            when: (answers) => answers.role === 'Manager'
-        },
-        {
-            type: "input",
-            message: "What is the employee's school?",
-            name: "school",
-            when: (answers) => answers.role === 'Intern'
-        },
-        {
-            type: "input",
-            message: "What is the employee's github?",
-            name: "github",
-            when: (answers) => answers.role === 'Engineer'
-        },
-        {
-            type: 'confirm',
-            name: 'again',
-            message: 'Enter another employee?',
-            default: true
-        }
-    ];
+  const prompts = [
+    {
+      type: "list",
+      message: "What is the employee's role?",
+      name: "role",
+      choices: ["Manager", "Engineer", "Intern"]
+    },
+    {
+      type: "input",
+      message: "What is the employee's name?",
+      name: "name"
+    },
+    {
+      type: "input",
+      message: "What is the employee's ID?",
+      name: "id"
+    },
+    {
+      type: "input",
+      message: "What is the employee's email?",
+      name: "email"
+    },
+    {
+      type: "input",
+      message: "What is the employee's office number?",
+      name: "office",
+      when: answers => answers.role === "Manager"
+    },
+    {
+      type: "input",
+      message: "What is the employee's school?",
+      name: "school",
+      when: answers => answers.role === "Intern"
+    },
+    {
+      type: "input",
+      message: "What is the employee's github?",
+      name: "github",
+      when: answers => answers.role === "Engineer"
+    },
+    {
+      type: "confirm",
+      name: "again",
+      message: "Enter another employee?",
+      default: true
+    }
+  ];
 
-    const { again, ...answers } = await inquirer.prompt(prompts);
-    const newInputs = [...inputs, answers];
-    return again ? collectInputs(newInputs) : newInputs;
+  const { again, ...answers } = await inquirer.prompt(prompts);
+  const newInputs = [...inputs, answers];
+  return again ? collectInputs(newInputs) : newInputs;
 };
 
 const main = async () => {
-    const team = await collectInputs();
-    console.log(team);
+  const team = await collectInputs();
+  console.log(team);
 
+  for (let i = 0; i < team.length; i++) {
+    const teamMember = await team[i];
 
-    for (let i = 0; i < team.length; i++) {
-        const teamMember = await team[i]
+    console.log("=====================");
+    console.log(teamMember);
 
-        console.log("=====================");
-        console.log(teamMember);
-
-        if (teamMember.role === "Manager") {
-            const html = `
+    if (teamMember.role === "Manager") {
+      const html = `
         
             <div class="card">
                 <div class="card-header">
@@ -94,11 +92,10 @@ const main = async () => {
                 </div>
             </div>
         
-`
-            appendFileAsync("index.html", html)
-        }
-        else if (teamMember.role === "Engineer") {
-            const html = `
+`;
+      appendFileAsync("index.html", html);
+    } else if (teamMember.role === "Engineer") {
+      const html = `
                     <div class="card">
                 <div class="card-header">
                     ${teamMember.name}
@@ -113,11 +110,10 @@ const main = async () => {
                 </div>
             </div>
         
-`
-            appendFileAsync("index.html", html)
-        }
-        else if (teamMember.role === "Intern") {
-            const html = `
+`;
+      appendFileAsync("index.html", html);
+    } else if (teamMember.role === "Intern") {
+      const html = `
             <div class="card">
                 <div class="card-header">
                     ${teamMember.name}
@@ -132,39 +128,16 @@ const main = async () => {
                 </div>
             </div>
       
-`
-            appendFileAsync("index.html", html)
-        }
-        else {
-            const html = `</body> </html>`
-            appendFileAsync("index.html", html)
-
-        }
-
+`;
+      appendFileAsync("index.html", html);
+    } else {
+      const html = `</body> </html>`;
+      appendFileAsync("index.html", html);
     }
+  }
 };
 
 main();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // const renderTeam = team => {
 
